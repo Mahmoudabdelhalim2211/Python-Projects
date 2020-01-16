c2683b8187434f83d7ebdef4655e156baccb87a5
Mahmoud Ashraf Abdelhalim <V18MHalim6@vodafone.com>
2020-01-16 15:40:13 +0200

    displaying goodbye message at the end of the code

--- bikeshare_final.py
+++ bikeshare_final.py
@@ -277,7 +277,8 @@ def main():
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
-            break
+            print('Thank you Goodbye')
+			break
 
 if __name__ == "__main__":
     main()
